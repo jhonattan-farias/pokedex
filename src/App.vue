e7a9668d343dcc3bdc2dea19a4c6180032813014
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2020-11-18 21:20:03 -0300

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app" v-for="(pokemon,index) in pokemons" :key="index">
+  <Pokemon :name="pokemon.name" :url="pokemon.url" :number="index"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Pokemon from './components/Pokemon';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+    },
+
+    data(){
+      return{
+        pokemons : []
+      }
+    },
+
+  created: async function(){
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+
+    console.log("Pokemons capturados")
+
+    this.pokemons = response.data.results
+
+    console.log(this.pokemons)
+  },
+
   }
-}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,53 @@
+<template>
+  <div style="display:flex; align-items:center; justify-content:center; flex-direction:column; ">
+    <span>{{number + 1}}</span>
+    <strong style="color:orangered; align-self:center;">
+      {{upper}}
+    </strong>
+    <small>
+      {{url}}
+    </small><br>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  
+  computed: {
+
+  upper:function(){
+      return this.name[0].toUpperCase() + this.name.slice(1)
+  },  
+  },
+
+    created: async function(){
+  
+      const response = await axios.get(this.url)
+      
+      this.pokemon.type = response.data.types[0].type.name
+      this.pokemon.front = response.data.sprites.front_default
+      this.pokemon.back = response.data.sprites.back_default
+        console.log(this.pokemon)
+    },
+
+    data(){
+      return{
+        pokemon:{}
+
+      }
+
+    },
+      
+  props:{
+      name:String,
+      url:String,
+      number:Number,
+  },
+
+}
+</script>
+
+<style>
+
+</style>
