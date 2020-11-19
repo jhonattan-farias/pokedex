49b9da885dca31cd584e38ada0f80dac2f406eff
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2020-11-19 18:29:08 -0300

    turn function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
+<div>
+  <div class="logo">
+  <img src="https://vibrant-yonath-715bf2.netlify.app/static/media/pokedex-logo.dc5fe4c4.svg" alt="">
+  </div>
 <div id="app" v-for="(pokemon,index) in pokemons" :key="index">
+
   <Pokemon :name="pokemon.name" :url="pokemon.url" :number="index"/>
+</div>
 </div>
 </template>
 
@@ -34,7 +40,22 @@ export default {
 </script>
 
 <style>
-#app {
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing:border-box;
+}
+#app{
+}
+div.logo{
+  background: rgba(255, 115, 0, 0.733);
+  width:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.logo img{
+  width:300px ;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,12 +1,25 @@
 <template>
-  <div style="display:flex; align-items:center; justify-content:center; flex-direction:column; ">
-    <span>{{number + 1}}</span>
-    <strong style="color:orangered; align-self:center;">
-      {{upper}}
-    </strong>
-    <small>
-      {{url}}
-    </small><br>
+  <div class="all" style="display:flex; align-items:center; justify-content:center; ">
+    <strong style="margin-right:5px; color:white;">{{number}}</strong>
+
+    <div class="pokemon">
+      <div class="img">
+        
+        <img  @click="changeImg" :src="pokeImg">
+        <p>Clique</p>
+      </div>
+      <div class="info">
+
+        <div class="name">
+         <strong>{{name}}</strong>
+        </div>
+
+        <div class="type">
+          <p>{{pokemon.type}}</p>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,6 +32,7 @@ export default {
   upper:function(){
       return this.name[0].toUpperCase() + this.name.slice(1)
   },  
+
   },
 
     created: async function(){
@@ -29,17 +43,40 @@ export default {
       this.pokemon.front = response.data.sprites.front_default
       this.pokemon.back = response.data.sprites.back_default
       
-        console.log(this.pokemon)
+      this.pokeImg = this.pokemon.front
     },
 
     data(){
+      
       return{
-        pokemon:{}
-
+        pokemon:{
+          type:'',
+          front:'',
+          back:'',
+          },
+          pokeImg:'',
+          
       }
+      
 
     },
-      
+      methods:{
+
+      changeImg: function(){
+
+        var front = this.pokemon.front
+        var back = this.pokemon.back
+
+        if(this.pokeImg === front){
+        this.pokeImg = back
+        }
+        else{
+         this.pokeImg = front
+        }
+
+      }
+        
+    },
   props:{
       name:String,
       url:String,
@@ -51,4 +88,69 @@ export default {
 
 <style>
 
+.all{
+  background: rgb(235, 235, 235);
+}
+
+.pokemon{
+  transition:.2s;
+  cursor: pointer;
+  width: 250px;
+  padding-bottom: 10px;
+}
+.pokemon:hover{
+transform: translateX(15px);
+}
+
+.img{
+  text-align: center;
+  background: rgb(234, 246, 248);
+  border-radius: 10px 10px 0 0;
+  margin-top: 10px;
+  width:250px;
+  padding-bottom: 10%;
+
+}
+
+img{
+width: 150px;
+height: 150px;
+
+margin-top: 2%;
+}
+
+.info{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background:#8CEBA2;
+  border-radius: 0 0 10px 10px;
+  padding: .5rem;
+}
+
+
+strong{
+  font-family: Lato;
+  font-size: 20pt;
+  color: #39486B ;
+  font-weight: bold ;
+}
+
+.type{
+  display: flex;
+  justify-content: center;
+
+  background: white;
+  width:70px;
+  border-radius:20px;
+
+  margin-top: 10px;
+  height: 25px;
+}
+p{
+  font-family: Lato;
+  font-weight: 400;
+  font-size: 13pt;
+}
 </style>
